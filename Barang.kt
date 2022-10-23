c108085c750946950dea85733226cc8439ee9654
Evi Nurastina <102676422+EviNurastina04@users.noreply.github.com>
2022-10-23 17:59:06 +0700

    Update Barang.kt

--- Barang.kt
+++ Barang.kt
@@ -3,9 +3,13 @@ package com.example.NotesApp
 class Barang {
 
     var id: Int? = null
+    //berfungsi sebagai primary key dan akan terhubung pada database pada saat dijalankan
     var name: String? = null
+    //menambahkan nama
     var jenis: String? = null
+    // menambahkan jenis barang
     var harga: String? = null
+    //menambahkan harga barang
 
     constructor(id: Int, name: String, jenis: String, harga:String){
         this.id = id
